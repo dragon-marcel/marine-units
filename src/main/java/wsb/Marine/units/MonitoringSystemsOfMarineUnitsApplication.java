2a7d482653dde72b4dab0da34f848c5358c7faa3
Marcel Dragon <dragon.marcel@o2.pl>
2022-10-04 09:53:56 +0200

    Init project

---
+++ src/main/java/wsb/Marine/units/MonitoringSystemsOfMarineUnitsApplication.java
@@ -0,0 +1,13 @@
+package wsb.Marine.units;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringSystemsOfMarineUnitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringSystemsOfMarineUnitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wsb/Marine/units/MonitoringSystemsOfMarineUnitsApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.Marine.units;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringSystemsOfMarineUnitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
